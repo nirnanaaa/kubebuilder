54cc93c8d657dba49ee040d9236b37d9c76c2d31
nirnanaaa <flo.fk44@gmail.com>
2019-06-04 07:18:36 +0200

    remove gopath for accessing binaries.

--- pkg/scaffold/v2/makefile.go
+++ pkg/scaffold/v2/makefile.go
@@ -102,8 +102,7 @@ docker-push:
 controller-gen:
 ifeq (, $(shell which controller-gen))
 	go get sigs.k8s.io/controller-tools/cmd/controller-gen@v0.2.0-beta.1
-CONTROLLER_GEN=$(shell go env GOPATH)/bin/controller-gen
-else
-CONTROLLER_GEN=$(shell which controller-gen)
 endif
+CONTROLLER_GEN=$(shell which controller-gen)
+
 `
